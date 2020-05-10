08aa9877096588cd4d1b8b8245b8143b906a65fa
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2020-05-10 10:05:27 -0400

    merge template files into extension

--- functions.php
+++ functions.php
@@ -10,7 +10,40 @@
  * Domain Path: /languages
  */
 
-// creates the block
+// Register Custom post type
 include( plugin_dir_path( __FILE__ ) . 'register/register-portfolio.php');
+
+// // include single and archive page for portfolio
+
+add_filter( 'template_include', 'add_portfolio_template', 99 );
+
+function add_portfolio_template( $template ) {
+   
+    if ( is_archive( 'portfolio' ) ) {
+    	 $file_name = 'archive-portfolio.php';
+
+        if ( locate_template( $file_name ) ) {
+            $template = locate_template( $file_name );
+        } else {
+            // Template not found in theme's folder, use plugin's template as a fallback
+            $template = dirname( __FILE__ ) . '/templates/' . $file_name;
+        }
+    }
+
+    if ( is_singular( 'portfolio' ) ) {
+    	 $file_name = 'single-portfolio.php';
+
+        if ( locate_template( $file_name ) ) {
+            $template = locate_template( $file_name );
+        } else {
+            // Template not found in theme's folder, use plugin's template as a fallback
+            $template = dirname( __FILE__ ) . '/templates/' . $file_name;
+        }
+	}
+
+
+    return $template;
+}
+
 
 ?>
---
+++ templates/parts/single-contentportfolio.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
+	<header class="entry-header">
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
+
+		if ( 'post' === get_post_type() ) :
+			?>
+			<div class="entry-meta">
+				<?php
+				sean_shea_development_posted_on();
+				sean_shea_development_posted_by();
+				?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			// Get the taxonomy's terms
+			$terms = get_terms(
+			    array(
+			        'taxonomy'   => 'portfolio-tags',
+			        'hide_empty' => false,
+			    )
+			);
+			
+			// Check if any term exists
+			if ( ! empty( $terms ) && is_array( $terms ) ) {
+			    // Run a loop and print them all
+			?>
+				<h2> Built With:</h2>
+				<?php 
+			    foreach ( $terms as $term ) { ?>
+			        <a href="<?php echo esc_url( get_term_link( $term ) ) ?>">
+			            <?php echo $term->name; ?>
+			        </a><?php
+			    }
+			} 
+		
+		the_content( sprintf(
+			wp_kses(
+				/* translators: %s: Name of current post. Only visible to screen readers */
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'sean-shea-development' ),
+				array(
+					'span' => array(
+						'class' => array(),
+					),
+				)
+			),
+			get_the_title()
+		) );
+
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sean-shea-development' ),
+			'after'  => '</div>',
+		) );
+		?>
+	</div><!-- .entry-content -->
+
+</article><!-- #post-<?php the_ID(); ?> -->
